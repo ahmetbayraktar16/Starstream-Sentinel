2de2397690177d546ee0ac1a8feb881d18327d63
Ahmet Bayraktar <apron4x@gmail.com>
2024-02-10 22:52:07 +0300

    Adjusted game balance and published the game for WebGL

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -13,7 +13,6 @@ public class LevelManager : MonoBehaviour
     {
         scoreKeeper = FindObjectOfType<ScoreKeeper>();
     }
-
     public void LoadGame()
     {
         scoreKeeper.ResetScore();
